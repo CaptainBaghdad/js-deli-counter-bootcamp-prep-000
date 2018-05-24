e4a127cc15be965c118c903558de2ba1ec5bdf15
Jason Pitts <juniorjr083@gmail.com>
2018-05-24 19:25:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ function currentLine(katzDeliLine){
   }
   
 var arr = [];
-var coun = 1;
-  for(var i = 0; i < katzDeliLine.length;i++){
-  arr.push(` `+coun + "." + " " + katzDeliLine[i]);
+
+for(var i = 0; i < katzDeliLine.length;i++){
+  arr.push(` `+ (i+1) + "." + " " + katzDeliLine[i]);
   
 } 
   return ("The line is currently:"+ arr);
